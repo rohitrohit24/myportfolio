1660e536c551496ad7e2640418723bff83d42c12
rohit kumar <rohitverma77270@gmail.com>
2026-07-15 14:40:14 +0530

    done link links

--- pwabuilder-adv-sw.js
+++ pwabuilder-adv-sw.js
@@ -1,3 +1,15 @@
+// Workbox library ko CDN se load karne ke liye scripts import karein
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-  precacheAndRoute([{"revision":"fbb130256844486b8ef4253928056108","url":"index.html"},{"revision":"be6dd660ed3151ef0d4d1f3e2bf709e4","url":"one.html"},{"revision":"063f40914e432d510232ee10f6fc3846","url":"style.css"},{"revision":"4a6cc4910dca1d025f3a9bf84d40b945","url":"style.html"},{"revision":"d3dda9cd554bb88b76e33a8ebe20f785","url":"two.html"}]);
+if (workbox) {
+  // Aapka precaching logic ab binna import error ke chalega
+  workbox.precaching.precacheAndRoute([
+    {"revision":"fbb130256844486b8ef4253928056108","url":"index.html"},
+    {"revision":"be6dd660ed3151ef0d4d1f3e2bf709e4","url":"one.html"},
+    {"revision":"063f40914e432d510232ee10f6fc3846","url":"style.css"},
+    {"revision":"4a6cc4910dca1d025f3a9bf84d40b945","url":"style.html"},
+    {"revision":"d3dda9cd554bb88b76e33a8ebe20f785","url":"two.html"}
+  ]);
+} else {
+  console.log('Workbox failed to load');
+}
