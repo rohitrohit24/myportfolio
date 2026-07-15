eee79d2eb985c999827f9a6a888869ba3ab5da22
rohit kumar <rohitverma77270@gmail.com>
2026-07-15 14:03:52 +0530

    done project

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"fbb130256844486b8ef4253928056108","url":"index.html"},{"revision":"be6dd660ed3151ef0d4d1f3e2bf709e4","url":"one.html"},{"revision":"063f40914e432d510232ee10f6fc3846","url":"style.css"},{"revision":"4a6cc4910dca1d025f3a9bf84d40b945","url":"style.html"},{"revision":"d3dda9cd554bb88b76e33a8ebe20f785","url":"two.html"}]);
